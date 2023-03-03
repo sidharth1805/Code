5a892c81c97edff9def0079e3d5966e4b835c1a3
sidharth1805 <sidharth.ramalingam@gmail.com>
2023-03-03 12:57:13 +0530

    black_fmt

--- Trees/mirror_view.py
+++ Trees/mirror_view.py
@@ -1,23 +1,24 @@
 from collections import deque
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
+
 class Solution:
-    def mirror_view(self,root :TreeNode) -> TreeNode:
+    def mirror_view(self, root: TreeNode) -> TreeNode:
         if not root:
             return None
         q = deque()
         q.append(root)
-
         while q:
             temp_node = q.popleft()
-            (temp_node.left,temp_node.right) = (temp_node.right,temp_node.left)
+            temp_node.left, temp_node.right = temp_node.right, temp_node.left
             if temp_node.left:
                 q.append(temp_node.left)
             if temp_node.right:
                 q.append(temp_node.right)
-        
         return root
