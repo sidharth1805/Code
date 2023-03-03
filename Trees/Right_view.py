c67b9da764a0d82f612c8b62b7ac8c370e1ac568
sidharth1805 <sidharth.ramalingam@gmail.com>
2023-03-03 12:47:37 +0530

    Right_view

---
+++ Trees/Right_view.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class solution:
+    def left_view(self, root):
+        q = deque()
+        q.append(root)
+        level_traversal = []
+        ans = []
+
+        while q:
+            temp_result = []
+            for i in range(len(q)):
+                temp_node = q.popleft()
+                temp_result.append(temp_node.val)
+                if temp_node.left:
+                    q.append(temp_node.left)
+                if temp_node.right:
+                    q.append(temp_node.right)
+
+            level_traversal.append(temp_result)
+
+        print(level_traversal)
+
+        for level in range(len(level_traversal)):
+            first_element = level_traversal[level][-1]
+            ans.append(first_element)
+
+        return ans
