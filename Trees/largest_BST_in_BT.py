fcc328f72705a332fe1cefabe18bd3d96c2454e4
sidharth1805 <sidharth.ramalingam@gmail.com>
2023-03-07 09:50:36 +0530

    black

--- Trees/largest_BST_in_BT.py
+++ Trees/largest_BST_in_BT.py
@@ -9,15 +9,19 @@ class Solution:
     def solve(self, A):
         def dfs(root):
             nonlocal result
+
             if not root:
                 return [True, float("inf"), float("-inf"), 0]
+
             l_isBST, leftMin, leftMax, leftSize = dfs(root.left)
             r_isBST, rightMin, rightMax, rightSize = dfs(root.right)
+
             if l_isBST and r_isBST and root.val > leftMax and root.val < rightMin:
                 result = max(result, leftSize + rightSize + 1)
                 leftMin = root.val if leftMin == float("inf") else leftMin
                 rightMax = root.val if rightMax == float("-inf") else rightMax
                 return [True, leftMin, rightMax, leftSize + rightSize + 1]
+
             else:
                 return [False, float("inf"), float("-inf"), 0]
 
