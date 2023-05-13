4b7d1492b27a4e44d1ddf40e4aec74fd374fdd7b
Sidharth Ramalingam <sidharth.ramalingam@gmail.com>
2023-05-13 11:56:31 +0530

    final

--- Graphs/rotten_oranges.py
+++ Graphs/rotten_oranges.py
@@ -15,4 +15,17 @@ class Solution:
                     q.append([i, j])
         dir = [[0,1],[0,-1],[1,0],[-1,0]]
         while q and fresh > 0 :
-            
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for row, column in dir:
+                    nr = r + row
+                    nc = c + column
+                    if(nr > 0 and nr != n and nc > 0 and nc != c and A[nr][nc] == 1):
+                        q.append([nr, nc])
+                        A[nr][nc] == 2
+                        fresh -= 1
+            time += 1
+        if fresh == 0 :
+            return time
+        else:
+            return -1
